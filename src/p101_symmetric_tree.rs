a6571716a35f53fbe7c438e8f54f647d9fbf6c6a
Sergey Chelak <sergey.chelak@gmail.com>
2023-02-17 22:11:38 +0200

    solution for #101 simplified

--- src/p101_symmetric_tree.rs
+++ src/p101_symmetric_tree.rs
@@ -13,55 +13,25 @@ impl Solution {
     pub fn is_symmetric(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
         if let Some(ref_cell) = root {
             let node = ref_cell.borrow();
-            let l_traverse = Self::bf_search(node.left.clone(), true);
-            let r_traverse = Self::bf_search(node.right.clone(), false);
-            if l_traverse.len() != r_traverse.len() {
-                false
-            } else {
-                l_traverse
-                    .iter()
-                    .zip(r_traverse.iter())
-                    .fold(true, |acc, (a, b)| acc && a == b)
-            }
+            Self::traverse(node.left.clone(), node.right.clone())
         } else {
             true
         }
     }
 
-    fn bf_search(node: Node, ltr: bool) -> Vec<Option<i32>> {
-        let mut current_level: Vec<Node> = vec![node];
-        let mut result: Vec<Option<i32>> = Vec::new();
-        loop {
-            let mut next_level: Vec<Node> = Vec::new();
-            for item in current_level.iter() {
-                if let Some(ref_cell) = item {
-                    let node = ref_cell.borrow();
-                    result.push(Some(node.val));
-                    if ltr {
-                        next_level.push(node.left.clone());
-                        next_level.push(node.right.clone());
-                    } else {
-                        next_level.push(node.right.clone());
-                        next_level.push(node.left.clone());
-                    }
-                } else {
-                    result.push(None);
-                }
-            }
-            if next_level.iter().filter(|elem| elem.is_some()).count() > 0 {
-                current_level = next_level;
+    fn traverse(left: Node, right: Node) -> bool {
+        if let (Some(ref_left), Some(ref_right)) = (&left, &right) {
+            let node_left = ref_left.borrow();
+            let node_right = ref_right.borrow();
+            if node_left.val == node_right.val {
+                Self::traverse(node_left.left.clone(), node_right.right.clone()) &&
+                Self::traverse(node_left.right.clone(), node_right.left.clone())
             } else {
-                break;
+                false
             }
+        } else {
+            left.is_none() && right.is_none()
         }
-        result
     }
-}
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn p101_case1() {
-        //
-    }
 }
