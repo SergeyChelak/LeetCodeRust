24a572d997e4196d52b88f0e34c0dc4d74318790
Sergey Chelak <sergey.chelak@gmail.com>
2023-02-19 18:19:04 +0200

    solved problem #108

---
+++ src/p108_convert_sorted_array_binary_tree.rs
@@ -0,0 +1,29 @@
+//
+// 108. Convert Sorted Array to Binary Search Tree
+// https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+//
+
+use std::rc::Rc;
+use std::cell::RefCell;
+use super::structs::tree_node::*;
+struct Solution;
+
+impl Solution {
+    pub fn sorted_array_to_bst(nums: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
+        Self::build_tree(&nums[..])
+    }
+
+    fn build_tree(nums: &[i32]) -> Option<Rc<RefCell<TreeNode>>> {
+        let len = nums.len();
+        if len > 0 {
+            let median = len / 2;
+            let mut node = TreeNode::new(nums[median]);
+            node.left = Self::build_tree(&nums[..median]);
+            node.right = Self::build_tree(&nums[median + 1..]);
+            node.wrapped()
+            //Some(Rc::new(RefCell::new(node)))
+        } else {
+            None
+        }
+    }
+}
