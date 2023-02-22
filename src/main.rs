93b960c408ea95cda2409487bf648e217ebc090c
Sergey Chelak <sergey.chelak@gmail.com>
2023-02-22 21:31:52 +0200

    solved problem #112

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod p101_symmetric_tree;
 mod p104_maximum_depth_binary_tree;
 mod p108_convert_sorted_array_binary_tree;
 mod p110_balanced_binary_tree;
+mod p112_path_sum;
 mod p202_happy_number;
 mod p67_add_binary;
 mod p94_binary_tree_inorder_traversal;
---
+++ src/p112_path_sum.rs
@@ -0,0 +1,44 @@
+//
+// 112. Path Sum
+// https://leetcode.com/problems/path-sum/
+//
+use std::rc::Rc;
+use std::cell::RefCell;
+
+use crate::structs::tree_node::*;
+struct Solution;
+
+impl Solution {
+    pub fn has_path_sum(root: Option<Rc<RefCell<TreeNode>>>, target_sum: i32) -> bool {
+        Self::find_sum(root, target_sum, None)
+    }
+
+    fn find_sum(root: Option<Rc<RefCell<TreeNode>>>, target_sum: i32, current_sum: Option<i32>) -> bool {
+        if let Some(root) = root {
+            let root = root.borrow();
+            let mut new_value = root.val;
+            if let Some(current_sum) = current_sum {
+                new_value += current_sum;
+            }
+            if new_value == target_sum && root.left.is_none() && root.right.is_none() {
+                true
+            } else {
+                Self::find_sum(root.left.clone(), target_sum, Some(new_value)) ||
+                Self::find_sum(root.right.clone(), target_sum, Some(new_value))
+            }
+    } else {
+            false
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn p112_test() {
+        let root = TreeNode::new(1).wrapped();
+        assert!(Solution::has_path_sum(root, 1));
+    }
+}
